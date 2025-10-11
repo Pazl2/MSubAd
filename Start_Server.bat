d2c33d6f18d255de2746f5c84422b931f067aac8
Pazl2 <pavel.yasinsky2006@gmail.com>
2025-10-11 11:45:01 +0300

    Changing logo and name of the site

--- Start_Server.bat
+++ Start_Server.bat
@@ -1,5 +1,5 @@
 @echo off
-set PROJECT_PATH="D:\5 сем - курсовая\DocAvia"
+set PROJECT_PATH="C:\Users\Pazl2\Desktop\University\5_sem\KURSACH\MSubAd"
 cd /d %PROJECT_PATH%
 npm run dev
 echo Сервер Запущен!
